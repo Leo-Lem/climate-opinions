8fe5d2fc864a96963aa6d3f53694a527f32865eb
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-12-28 12:39:32 +0000

    Fixed preprocessing.

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,6 @@
-from pandas import read_csv, DataFrame, Series
+from pandas import read_csv, DataFrame
 from os import path
-from torch import Tensor, tensor
+from torch import Tensor, tensor, long
 from torch.utils.data import Dataset
 from transformers import BertTokenizer
 
@@ -10,7 +10,6 @@ from __params__ import DATA_PATH, OUT_PATH
 class ClimateChangeOpinions(Dataset):
     DATA_FILE = path.join(DATA_PATH, "data.csv")
     PREPROCESSED_FILE = path.join(OUT_PATH, "preprocessed.csv")
-    ENCODED_FILE = path.join(OUT_PATH, "encoded.csv")
 
     MAX_LENGTH = 128
 
@@ -20,56 +19,49 @@ class ClimateChangeOpinions(Dataset):
 
         self.data = self.__preprocess__() if data is None else data
 
-        if ("input_ids", "attention_mask") not in self.data.columns:
-            self.data = self.__encode__()
-
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, i: int) -> tuple[Tensor, Tensor, int]:
         """ Return the input_ids, attention_mask and sentiment of the i-th message as a tensor. """
-        return tensor(self.data.loc[i, "input_ids"]), \
-            tensor(self.data.loc[i, "attention_mask"]), \
-            tensor(self.data.loc[i, "sentiment"])
+        input_ids, attention_mask = self.__encode__(self.data.at[i, "message"])
+        target = self.__target__(self.data.at[i, "sentiment"])
+        return input_ids, attention_mask, target
 
     def __preprocess__(self) -> DataFrame:
-        """ Load from csv and remove factual label. """
+        """ Load from csv, remove factual label, and shift to positive numbers. """
         if path.exists(self.PREPROCESSED_FILE):
-            return read_csv(self.PREPROCESSED_FILE, dtype={"sentiment": int,
-                                                           "message": str,
-                                                           "tweetid": int})
+            return read_csv(self.PREPROCESSED_FILE,
+                            dtype={"sentiment": int,
+                                   "message": str,
+                                   "tweetid": int})
 
-        preprocessed = read_csv(self.DATA_FILE, dtype={"sentiment": int,
-                                                       "message": str,
-                                                       "tweetid": int})
-        preprocessed.replace({"sentiment": {2: 1}}, inplace=True)
-        preprocessed.to_csv(self.PREPROCESSED_FILE)
+        data = read_csv(self.DATA_FILE,
+                        dtype={"sentiment": int,
+                               "message": str,
+                               "tweetid": int})
+        preprocessed = data.replace({"sentiment": {2: 1}})
+        preprocessed = preprocessed.replace({"sentiment": {1: 2, 0: 1, -1: 0}})
+        preprocessed.to_csv(self.PREPROCESSED_FILE, index=True)
         return preprocessed
 
-    def __encode__(self) -> DataFrame:
-        """ Encode the messages using the tokenizer. """
-        if path.exists(self.ENCODED_FILE):
-            return read_csv(self.ENCODED_FILE, dtype={"sentiment": int,
-                                                      "message": str,
-                                                      "tweetid": int,
-                                                      "input_ids": Tensor,
-                                                      "attention_mask": Tensor})
+    def __encode__(self, message: str) -> tuple[Tensor, Tensor]:
+        """ Encode a single message into input_ids and attention_mask. """
+        encoding = self.tokenizer.encode_plus(message,
+                                              add_special_tokens=True,
+                                              max_length=self.MAX_LENGTH,
+                                              padding="max_length",
+                                              truncation=True,
+                                              return_token_type_ids=False,
+                                              return_attention_mask=True,
+                                              return_tensors="pt")
+        return encoding["input_ids"].flatten(), encoding["attention_mask"].flatten()
 
-        encoded = self.data.copy()
-        encoded[["input_ids", "attention_mask"]] = self.data["message"]\
-            .apply(lambda x: self.tokenizer.encode_plus(x,
-                                                        add_special_tokens=True,
-                                                        max_length=self.MAX_LENGTH,
-                                                        padding=True,
-                                                        truncation=True,
-                                                        return_token_type_ids=False,
-                                                        return_attention_mask=True,
-                                                        return_tensors="pt"))\
-            .apply(Series)
-        encoded.to_csv(self.ENCODED_FILE)
-        return encoded
+    def __target__(self, sentiment: int) -> Tensor:
+        """ Return the target tensor for the sentiment. """
+        return tensor(sentiment, dtype=long)
 
-    def split(self, train_frac: float = 0.8, val_frac: float = 0.1, test_frac: float = 0.1) -> tuple["ClimateChangeOpinions", "ClimateChangeOpinions", "ClimateChangeOpinions"]:
+    def split(self, train_frac: float = .8, val_frac: float = .1, test_frac: float = .1) -> tuple["ClimateChangeOpinions", "ClimateChangeOpinions", "ClimateChangeOpinions"]:
         """ Split the dataset into three parts. """
         assert train_frac + val_frac + test_frac == 1
         assert 0 < train_frac < 1 and 0 < val_frac < 1 and 0 < test_frac < 1
@@ -77,6 +69,6 @@ class ClimateChangeOpinions(Dataset):
         train = self.data.sample(frac=train_frac)
         val = self.data.drop(train.index).sample(frac=val_frac/(1-train_frac))
         test = self.data.drop(train.index).drop(val.index)
-        return ClimateChangeOpinions(self.model, train), \
-            ClimateChangeOpinions(self.model, val), \
-            ClimateChangeOpinions(self.model, test)
+        return ClimateChangeOpinions(self.model, train.reset_index(drop=True)), \
+            ClimateChangeOpinions(self.model, val.reset_index(drop=True)), \
+            ClimateChangeOpinions(self.model, test.reset_index(drop=True))
--- src/train.py
+++ src/train.py
@@ -16,11 +16,11 @@ def train(model: Module, train: Dataset, validation: Dataset):
 
     for epoch in (epochs := trange(EPOCHS, desc="Epoch", unit="epoch")):
         model.train()
-        for labels, input_ids, attention_mask in (batches := tqdm(train_loader, desc="Training", unit="batch", leave=False)):
+        for input_ids, attention_mask, label in (batches := tqdm(train_loader, desc="Training", unit="batch", leave=False)):
             optimizer.zero_grad()
             output = model(input_ids, attention_mask)
 
-            loss: Tensor = loss_fn(output, labels)
+            loss: Tensor = loss_fn(output, label)
             loss.backward()
             optimizer.step()
 
@@ -29,12 +29,12 @@ def train(model: Module, train: Dataset, validation: Dataset):
         model.eval()
         val_loss, val_correct = 0, 0
         with no_grad():
-            for labels, input_ids, attention_mask in (batches := tqdm(val_loader, desc="Validation", unit="batch", leave=False)):
+            for input_ids, attention_mask, label in (batches := tqdm(val_loader, desc="Validation", unit="batch", leave=False)):
                 prediction = model.predict(input_ids, attention_mask)
 
-                loss: Tensor = loss_fn(prediction, labels)
+                loss: Tensor = loss_fn(prediction, label)
                 val_loss += loss.item()
-                val_correct += sum(prediction == labels).item()
+                val_correct += sum(prediction == label).item()
 
                 batches.set_postfix(loss=loss.item())
 
